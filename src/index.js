79fc0896c9367f4cd2b5d0fc7ba1eedce7196b9c
miu-cz <0151300@gmail.com>
2024-07-30 00:03:42 +0200

    feat: job is done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
-  
+    let numStr = n.toString();
+
+    if (n < 0) {
+        numStr = numStr.substring(1);
+    }
+
+    let reversedStr = numStr.split('').reverse().join('');
+    let reversedNum = parseInt(reversedStr, 10);
+
+    if (n < 0) {
+        reversedStr = -reversedStr;
+    }
+
+    return reversedNum;
 }
